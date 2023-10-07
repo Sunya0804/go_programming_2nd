0a6e88adfa3fae76739a55e7e499a20118cd63e1
Woojin <sunya0804@kma.ac.kr>
2023-10-08 00:31:01 +0900

    q2 풀이 완료

--- q2.go
+++ q2.go
@@ -40,28 +40,26 @@ func sum(num int, fileName string) int {
 	checkError(err)
 
 	// Create buffered channel
-	intsChan := make(chan int, len(ints))
-
-	// Send integers to channel
-	for _, num := range ints {
-		intsChan <- num
-	}
-
-	// Create buffered channel
-	sumChan := make(chan int, num)
+	intsBuffer := len(ints) / num  // Buffer size. 파이썬과 다르게 몫을 반환
+	sumChan := make(chan int, num) // 부분합들을 저장할 채널
 
 	// Create workers
 	for i := 0; i < num; i++ {
+		intsChan := make(chan int, intsBuffer)
+		for j := 0; j < intsBuffer; j++ {
+			intsChan <- ints[i*intsBuffer+j]
+		}
 		go sumWorker(intsChan, sumChan)
-	}
 
-	// Close channel
-	close(intsChan)
+		// Close channel
+		close(intsChan)
+	}
 
 	// Sum integers
 	sum := 0
 	for i := 0; i < num; i++ {
 		sum += <-sumChan
+		// fmt.Println("sum :", sum)
 	}
 
 	// Close channel
